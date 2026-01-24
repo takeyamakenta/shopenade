b6faee387d4722507bfc5ef032287eb50828cf3d
takeyamakenta <takeyamakenta@gmail.com>
2026-01-25 08:33:03 +0900

    ナビボタンをレスポンシブにする

--- src/layouts/NavFooter.tsx
+++ src/layouts/NavFooter.tsx
@@ -14,10 +14,12 @@ function NavFooter() {
             <nav class={`${styles.footer} bg-gray-800 text-yellow-100`}>
                 <div class="flex flex-row items-center justify-center gap-4 px-2">
                     <Button variant="nav" size="sm" onClick={() => navigate("/search_by_map")}>
-                        <MapPinned class="h-8 w-8" />
+                        <MapPinned class="h-6 w-6 sm:h-5 sm:w-5" />
+                        <span class="hidden sm:inline ml-2">地図から探す</span>
                     </Button>
                     <Button variant="nav" size="sm" onClick={() => navigate("/search_by_hiragana")}>
-                        <ArrowDownAZ class="h-8 w-8" />
+                        <ArrowDownAZ class="h-6 w-6 sm:h-5 sm:w-5" />
+                        <span class="hidden sm:inline ml-2">名前から探す</span>
                     </Button>
                 </div>
             </nav>
